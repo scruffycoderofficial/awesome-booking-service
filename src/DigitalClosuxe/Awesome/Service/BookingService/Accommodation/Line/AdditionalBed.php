7270819e79e29a5883f5b53e2778fed205e888d0
Luyanda Siko <sikoluyanda@gmail.com>
2020-07-18 05:34:21 +0200

    Cleaned up source files and added some comments where necessary

--- src/DigitalClosuxe/Awesome/Service/BookingService/Accommodation/Line/AdditionalBed.php
+++ src/DigitalClosuxe/Awesome/Service/BookingService/Accommodation/Line/AdditionalBed.php
@@ -4,15 +4,27 @@ namespace DigitalClosuxe\Awesome\Service\BookingService\Accommodation\Line;
 
 use DigitalClosuxe\Awesome\Service\BookingService\Decorator\AccommodationBookingsService;
 
+/**
+ * An Accomodation LineItem interface that could also be refered to as ServiceFeaturedItem.
+ *
+ * @author luyandasiko
+ */
 class AdditionalBed extends AccommodationBookingsService
 {
+    /**
+     * Additional charge for this Line Item.
+     *
+     * @var int
+     */
     private const ADDITIONAL_AMOUNT = 30;
 
+    /** [{@inheritdoc}] */
     public function getDescription()
     {
         return sprintf('%s with an extra bed', $this->bookingService->getDescription());
     }
 
+    /** [{@inheritdoc}] */
     public function calculatePrice()
     {
         return $this->bookingService->calculatePrice() + self::ADDITIONAL_AMOUNT;
